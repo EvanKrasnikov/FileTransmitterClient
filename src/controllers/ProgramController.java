0466500dcb8689c3d7acf6206f7a1565a0a987b7
i.krasnikov <geographer29@yandex.ru>
2018-01-24 13:48:49 +0300

    Added "Pick a file feature". Size and last modification time formatted correctly

--- src/controllers/ProgramController.java
+++ src/controllers/ProgramController.java
@@ -1,9 +1,7 @@
 package controllers;
 
-import client.File;
+import client.FileEntry;
 import com.jfoenix.controls.*;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -11,16 +9,23 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.TreeItemPropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ProgramController implements Initializable{
+    private TreeItem<FileEntry> itemRoot;
+
     @FXML
     private AnchorPane menuPanel;
     @FXML
-    private JFXTreeTableView<File> tableView;
+    private JFXTreeTableView<FileEntry> tableView;
     @FXML
     private JFXButton addFiles;
     @FXML
@@ -29,49 +34,53 @@ public class ProgramController implements Initializable{
     private BorderPane borderpane;
     @FXML
     private void add(ActionEvent event) throws IOException{
-        //todo
+        FileChooser chooser = new FileChooser();
+        List<File> files = chooser.showOpenMultipleDialog(tableView.getScene().getWindow());
+
+        for (File f: files){
+            FileEntry fileEntry = new FileEntry(f.getName(),getFormattedSizeOfFile(f.length()),getFormattedDate(f.lastModified()));
+            TreeItem<FileEntry> item = new TreeItem<>(fileEntry);
+            itemRoot.getChildren().add(item);
+            System.out.println(fileEntry.getFileName() + " " + fileEntry.getSize() + " " + fileEntry.getEditionTime());
+        }
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        JFXTreeTableColumn<File, String> nameColumn = new JFXTreeTableColumn<>("Name");
-        JFXTreeTableColumn<File, String> sizeColumn = new JFXTreeTableColumn<>("Size");
-        JFXTreeTableColumn<File, String> editionTimeColumn = new JFXTreeTableColumn<>("Last change");
+        JFXTreeTableColumn<FileEntry, String> nameColumn = new JFXTreeTableColumn<>("Name");
+        JFXTreeTableColumn<FileEntry, String> sizeColumn = new JFXTreeTableColumn<>("Size");
+        JFXTreeTableColumn<FileEntry, String> editionTimeColumn = new JFXTreeTableColumn<>("Last change");
 
         nameColumn.setPrefWidth(300);
         sizeColumn.setPrefWidth(100);
         editionTimeColumn.setPrefWidth(200);
 
-        // Get value from property of File class
+        // Get value from property of FileEntry class
         nameColumn.setCellValueFactory(new TreeItemPropertyValueFactory<>("Name"));
         sizeColumn.setCellValueFactory(new TreeItemPropertyValueFactory<>("Size"));
         editionTimeColumn.setCellValueFactory(new TreeItemPropertyValueFactory<>("Time"));
 
         tableView.getColumns().addAll(nameColumn, sizeColumn, editionTimeColumn);
 
-        TreeItem<File> itemRoot = new TreeItem<>();
-
-        for (File f: getFileList()){
-            itemRoot.getChildren().add(new TreeItem<>(f));
-            System.out.println(f.getFileName() + " " + f.getSize() + " " + f.getEditionTime());
-        }
-
+        itemRoot = new TreeItem<>();
         tableView.setRoot(itemRoot);
         tableView.setShowRoot(false);
         tableView.setEditable(false);
     }
 
-    private ObservableList<File> getFileList() {
-        ObservableList<File> users = FXCollections.observableArrayList();
-        users.add(new File("Computer_Department", "23","CD_1"));
-        users.add(new File("Sales Department", "22","Employee 1"));
-        users.add(new File("Sales Department", "22","Employee 2"));
-        users.add(new File("Sales Department", "25","Employee 4"));
-        users.add(new File("Sales Department", "25","Employee 5"));
-        users.add(new File("IT Department", "42","ID 2"));
-        users.add(new File("HR Department", "22","HR 1"));
-        users.add(new File("HR Department", "22","HR 2"));
-        return users;
+    private String getFormattedSizeOfFile(long size){
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+        float sizeKb = 1024.0f;
+        float sizeMb = sizeKb * sizeKb;
+        float sizeGb = sizeMb * sizeKb;
+
+        if (size > sizeMb) return decimalFormat.format(size / sizeMb) + " Mb";
+        else if (size > sizeGb) return decimalFormat.format(size / sizeGb) + " Gb";
+        return decimalFormat.format(size / sizeKb) + " Kb";
     }
 
+    private String getFormattedDate(long date){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        return dateFormat.format(date);
+    }
 }
