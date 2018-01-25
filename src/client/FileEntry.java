0857daaaaef6cba563fd08e7fc8540d1c89fcb11
i.krasnikov <geographer29@yandex.ru>
2018-01-25 14:26:23 +0300

    FileEntry class simplified

--- src/client/FileEntry.java
+++ src/client/FileEntry.java
@@ -1,53 +1,27 @@
 package client;
 
 import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
 
 public class FileEntry extends RecursiveTreeObject<FileEntry> {
-    private StringProperty fileName;
-    private StringProperty size;
-    private StringProperty editionTime;
+    private String fileName;
+    private String size;
+    private String editionTime;
 
     public FileEntry(String fileName, String size, String editionTime) {
-        this.fileName = new SimpleStringProperty(fileName) ;
-        this.size = new SimpleStringProperty(size);
-        this.editionTime = new SimpleStringProperty(editionTime);
+        this.fileName = fileName ;
+        this.size = size;
+        this.editionTime = editionTime;
     }
 
     public String getFileName() {
-        return fileName.get();
-    }
-
-    public StringProperty fileNameProperty() {
         return fileName;
     }
 
-    public void setFileName(String fileName) {
-        this.fileName.set(fileName);
-    }
-
     public String getSize() {
-        return size.get();
-    }
-
-    public StringProperty sizeProperty() {
         return size;
     }
 
-    public void setSize(String size) {
-        this.size.set(size);
-    }
-
     public String getEditionTime() {
-        return editionTime.get();
-    }
-
-    public StringProperty editionTimeProperty() {
         return editionTime;
-    }
-
-    public void setEditionTime(String editionTime) {
-        this.editionTime.set(editionTime);
     }
 }
