eaaa0856887eb8da570c04a02cccd3410bc8873b
Ivan Krasnikov <geographer29@yandex.ru>
2018-02-22 23:29:59 +0300

    Added some messanges for push notifications

---
+++ src/utils/ClientMessages.java
@@ -0,0 +1,10 @@
+package utils;
+
+public interface ClientMessages {
+    String NO_SUCH_USER = "Enter a unique loginview, please";
+    String CORRECT_PASS = "Login and pass are correct";
+    String INCORRECT_PASS = "Login or password is incorrect. Try again";
+    String LOGIN_IS_OCCUPIED = "Login is occupied. Enter a unique login, please ";
+    String TRANSITION_COMPLETE = "Files list received";
+    String GET_FILES = "List of files received";
+}
