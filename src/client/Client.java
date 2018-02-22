d5aaad4d7ca8b76a423ad6d84831d02cec9e05dc
Ivan Krasnikov <geographer29@yandex.ru>
2018-02-22 23:48:50 +0300

    Class Client reworked

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,31 +1,29 @@
 package client;
 
-import client.sync.Receiver;
-import client.sync.Sender;
-
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 
 public class Client  {
     private Selector selector;
-    private SocketChannel channel;
-    private static final String ADRESS = "localhost";
+    private SocketChannel client;
+    private static final String ADDRESS = "localhost";
     private static final int PORT = 8189;
-    protected Boolean isAuthentified = false;
+    protected Boolean isAuthenticated = false;
     protected Boolean terminateConnection = false;
 
     public Client(){
         try {
             selector = Selector.open();
-            channel = SocketChannel.open();
-            channel.configureBlocking(false);
-            channel.connect(new InetSocketAddress(ADRESS,PORT));
-            channel.register(selector, SelectionKey.OP_CONNECT, SelectionKey.OP_READ);
-            if (channel.isConnected()){
-                Receiver receiver = new Receiver(channel);
+            client = SocketChannel.open();
+            client.configureBlocking(false);
+            client.connect(new InetSocketAddress(ADDRESS,PORT));
+            client.register(selector, client.validOps());
+
+            if (client.isConnected()){
+                Receiver receiver = new Receiver(client);
                 while (!terminateConnection){
                     receiver.receiveMessage();
                 }
@@ -36,15 +34,15 @@ public class Client  {
     }
 
     public void sendMessage(String message){
-        Sender sender = new Sender(channel);
+        Sender sender = new Sender(client);
         sender.sendMessage(message);
     }
 
-    protected void setIsAuthentified(Boolean isAuthentified){
-        this.isAuthentified = isAuthentified;
+    protected void setIsAuthenticated(Boolean isAuthenticated){
+        this.isAuthenticated = isAuthenticated;
     }
 
     public SocketChannel getChannel() {
-        return channel;
+        return client;
     }
 }
