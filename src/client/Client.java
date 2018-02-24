0b134842dd7e733f20061b0ab90d32e77eab9045
Ivan Krasnikov <geographer29@yandex.ru>
2018-02-24 18:42:37 +0300

    Client class reworked

--- src/client/Client.java
+++ src/client/Client.java
@@ -2,47 +2,79 @@ package client;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.util.concurrent.ConcurrentLinkedDeque;
 
 public class Client  {
     private Selector selector;
-    private SocketChannel client;
+    private SocketChannel channel;
     private static final String ADDRESS = "localhost";
     private static final int PORT = 8189;
     protected Boolean isAuthenticated = false;
-    protected Boolean terminateConnection = false;
+    private ByteBuffer buffer = ByteBuffer.allocateDirect(BUFFER_SIZE);
+    private static int BUFFER_SIZE = 1024;
 
     public Client(){
         try {
             selector = Selector.open();
-            client = SocketChannel.open();
-            client.configureBlocking(false);
-            client.connect(new InetSocketAddress(ADDRESS,PORT));
-            client.register(selector, client.validOps());
+            channel = SocketChannel.open();
+            channel.configureBlocking(false);
+            channel.connect(new InetSocketAddress(ADDRESS,PORT));
+            channel.register(selector, channel.validOps());
 
-            if (client.isConnected()){
-                Receiver receiver = new Receiver(client);
-                while (!terminateConnection){
-                    receiver.receiveMessage();
-                }
+            if (channel.isConnected()){
+                while (true)
+                    receiveMessage();
             }
         } catch (IOException e) {
             System.err.println("Client can't connect to the server");
         }
     }
 
-    public void sendMessage(String message){
-        Sender sender = new Sender(client);
-        sender.sendMessage(message);
+    protected void sendMessage(byte[] bytes){
+        try {
+            buffer.get(bytes);
+            channel.write(buffer);
+            buffer.clear();
+        } catch (IOException e) {
+            System.err.println("Can't send message");
+        }
+    }
+
+    private void receiveMessage(){ // Receive a message
+        String result = "";
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try {
+            while (channel.read(buffer) > 0){
+                buffer.flip();
+                channel.read(buffer);
+                stringBuilder.append(result);
+                stringBuilder.append(new String(buffer.array()).trim());
+                buffer.clear();
+            }
+        } catch (IOException e) {
+            System.err.println("Can't send message");
+        }
+
+        String[] strings = result.split("\\s");
+        ConcurrentLinkedDeque<String> queue = new ConcurrentLinkedDeque<>();
+
+        for (String s: strings){
+            queue.push(s);
+        }
+
+        BaseHandler handler = new BaseHandler();
+        handler.parseMessage(queue);
     }
 
     protected void setIsAuthenticated(Boolean isAuthenticated){
         this.isAuthenticated = isAuthenticated;
     }
 
-    public SocketChannel getChannel() {
-        return client;
+    protected SocketChannel getChannel() {
+        return channel;
     }
 }
