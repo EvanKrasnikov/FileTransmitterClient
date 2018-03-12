122122959d0d2902f9426b34c6893ff59af90509
i.krasnikov <geographer29@yandex.ru>
2018-03-12 18:45:16 +0300

    Optimized Main class

--- src/controllers/Main.java
+++ src/controllers/Main.java
@@ -1,23 +1,13 @@
 package controllers;
 
-import client.Client;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/resources/Login.fxml"));
-        Scene scene = new Scene(root, 600, 400);
-        primaryStage.setScene(scene);
-        primaryStage.setResizable(false);
-        primaryStage.show();
-
-        new Client();
+       new LoginController();
     }
 
     public static void main(String[] args) {
